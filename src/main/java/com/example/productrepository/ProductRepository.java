c1448f93c0372fb97d6260f0957f69b977cebe16
neuefische <info@neuefische.de>
2023-09-17 18:47:32 +0200

    add find and remove methods

--- src/main/java/com/example/productrepository/ProductRepository.java
+++ src/main/java/com/example/productrepository/ProductRepository.java
@@ -1,6 +1,13 @@
 package com.example.productrepository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
+
+    Product findProductBy(String id);
+
+    void removeProductBy(String id);
+
 }

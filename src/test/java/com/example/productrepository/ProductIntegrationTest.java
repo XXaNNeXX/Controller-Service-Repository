533c6bda92b5c39b1fd60d7c47b875e055c9b0a9
neuefische <info@neuefische.de>
2023-09-18 17:00:57 +0200

    add removeProductTest

--- src/test/java/com/example/productrepository/ProductIntegrationTest.java
+++ src/test/java/com/example/productrepository/ProductIntegrationTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -18,21 +18,82 @@ public class ProductIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
-    //@Autowired
-    //ProductRepository productRepository;
+    @Autowired
+    ProductRepository productRepository;
 
     @Test
     @DirtiesContext
     void getAllProductsTest() throws Exception {
-        //productRepository.save(new Product("1", "ProductTitle", 100));
+        productRepository.save(new Product("1", "ProductTitle", 100));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/products"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
-                        []
+                        [
+                            {
+                                "id": "1",
+                                "title": "ProductTitle",
+                                "price": 100
+                            }
+                        ]
                         """));
     }
 
+    @Test
+    @DirtiesContext
+    void addProductTest() throws Exception {
 
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("""
+                            {
+                                "title": "test-title",
+                                "price": 100
+                            }
+                            """))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                                            {
+                                                "title": "test-title",
+                                                "price": 100
+                                            }
+                                            """))
+                .andExpect(jsonPath("$.id").isNotEmpty());
+    }
+
+    @Test
+    @DirtiesContext
+    void findProductByIdTest() throws Exception {
+        productRepository.save(new Product("1", "ProductTitle", 100));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/products/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            {
+                                "id": "1",
+                                "title": "ProductTitle",
+                                "price": 100
+                            }
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    void removeProductTest() throws Exception {
+        productRepository.save(new Product("1", "ProductTitle", 100));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/products/1")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("""
+                            {
+                                "id": "1",
+                                "title": "ProductTitle",
+                                "price": 100
+                            }
+                            """))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").doesNotExist());
+
+    }
 
 }
